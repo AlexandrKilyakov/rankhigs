4c91023774ce21102e6f6ba0291ae3853f11d567
AlexandrKilyakov <76633175+AlexandrKilyakov@users.noreply.github.com>
2022-09-22 12:05:00 +0300

    v 0.3

--- assets/js/originals/selected.js
+++ assets/js/originals/selected.js
@@ -2,12 +2,28 @@ document.addEventListener("DOMContentLoaded", () => {
   const selects = document.querySelectorAll("label:not(for) select");
 
   if (selects) {
-    selects.forEach((select) => {
-      addSelected(select);
-      select.addEventListener("change", () => {
-        addSelected(select);
-      });
-    });
+    let timer = setInterval(() => {
+      if (document.querySelector(".select2")) {
+        clearInterval(timer);
+        selects.forEach((select) => {
+          const select2 = select.parentElement.querySelector(".select2");
+          const select2Rendered = select2.querySelector(
+            ".select2-selection__rendered"
+          );
+
+          addSelected(select);
+          select.addEventListener("change", () => {
+            addSelected(select);
+          });
+
+          if (select2Rendered.title != "") {
+            if (!select2.classList.contains("selected")) {
+              select2.classList.add("selected");
+            }
+          }
+        });
+      }
+    }, 100);
   }
 
   function addSelected(select) {
