7b01164115342ec74f83fd450b7c82e97fd8daac
AlexandrKilyakov <76633175+AlexandrKilyakov@users.noreply.github.com>
2022-09-23 15:53:33 +0300

    v 0.6

--- assets/js/originals/header-margin.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const header = document.querySelector(".main-header");
-
-  if (header) {
-    header.nextElementSibling.style.marginTop = `${header.offsetHeight}px`;
-  }
-});
